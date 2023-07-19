22a892b068b57961aeffdcbdee61839a7b5a3677
Ahmad Ropai <akhmadrofai@gmail.com>
2023-07-19 08:42:20 +0700

    add footer

---
+++ components/layout/Footer.tsx
@@ -0,0 +1,138 @@
+import Image from "next/image";
+import { useState } from "react";
+import { FiInstagram, FiYoutube } from "react-icons/fi";
+
+const Footer = () => {
+  const date = new Date();
+  const [year, setYear] = useState(date.getFullYear());
+  return (
+    <>
+      <div className="bg-gray-900">
+        <div className="px-4 pt-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
+          <div className="grid row-gap-10 mb-8 lg:grid-cols-6">
+            <div className="grid grid-cols-2 gap-5 row-gap-8 lg:col-span-4 md:grid-cols-4">
+              <div className="col-span-2">
+                <div className="space-y-2">
+                  <Image
+                    width={100}
+                    height={100}
+                    src="/img/logo-pmpp.webp"
+                    alt="logo"
+                    className="lg:w-1/8 w-1/5"
+                  />
+                  <h1 className="h-1 bold text-white lg:text-4xl text-xl">
+                    PMPP
+                  </h1>
+                </div>
+              </div>
+
+              <div>
+                <p className="font-medium tracking-wide text-gray-300 text-sm lg:text-base">
+                  Beranda
+                </p>
+                <ul className="mt-2 space-y-2 text-sm lg:text-base">
+                  <li>
+                    <a
+                      href="/report"
+                      className="text-gray-500 transition-colors duration-300 hover:text-teal-400"
+                    >
+                      Sejarah
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="/"
+                      className="text-gray-500 transition-colors duration-300 hover:text-teal-400"
+                    >
+                      Tugas Pokok
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="/register"
+                      className="text-gray-500 transition-colors duration-300 hover:text-teal-400"
+                    >
+                      Struktur organisasi
+                    </a>
+                  </li>
+                </ul>
+              </div>
+              <div className="text-sm lg:text-base col-span-1">
+                <p className="font-medium tracking-wide text-gray-300">
+                  Aktifitas
+                </p>
+                <ul className="mt-2 space-y-2">
+                  <li>
+                    <a
+                      href="/#faq-section"
+                      className="text-gray-500 transition-colors duration-300 hover:text-teal-400"
+                    >
+                      Komandan dari masa ke masa
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="/term-and-condition"
+                      className="text-gray-500 transition-colors duration-300 hover:text-teal-400"
+                    >
+                      Kegiatan PMPP TNI
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="/term-and-condition"
+                      className="text-gray-500 transition-colors duration-300 hover:text-teal-400"
+                    >
+                      Kegiatan Kontingen Garuda
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="md:max-w-md lg:col-span-2">
+              <span className="text-base font-medium tracking-wide text-gray-300">
+                Subscribe for updates
+              </span>
+              <form className="flex flex-col mt-4 md:flex-row">
+                <input
+                  placeholder="Email"
+                  required
+                  type="text"
+                  className="flex-grow w-full h-12 px-4 mb-3 transition duration-200 bg-white border border-gray-300 rounded shadow-sm appearance-none md:mr-2 md:mb-0 focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline"
+                />
+                <button
+                  type="submit"
+                  className="inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-cyan-800 hover:bg-cyan-700 focus:shadow-outline focus:outline-none"
+                >
+                  Subscribe
+                </button>
+              </form>
+            </div>
+          </div>
+          <div className="flex flex-col justify-between pt-5 pb-10 border-t border-gray-800 sm:flex-row">
+            <p className="text-xs text-gray-500 lg:text-sm">
+              © Copyright {year} IAF PKC — Pusat Misi Pemeliharaan Perdamaian
+              TNI (PMPP TNI). All rights reserved.
+            </p>
+            <div className="flex items-center mt-4 space-x-4 sm:mt-0">
+              <a
+                href="/"
+                className="text-gray-500 transition-colors duration-300 hover:text-teal-400"
+              >
+                <FiYoutube size={24} className="stroke-current hover:text-teal-400" />
+              </a>
+              <a
+                href="/"
+                className="text-gray-500 transition-colors duration-300 hover:text-teal-400"
+              >
+                <FiInstagram size={24} className="stroke-current hover:text-teal-400" />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Footer;
---
+++ components/layout/Navbar.tsx
@@ -0,0 +1,108 @@
+import { useState } from "react";
+import clsx from "clsx";
+import Link from "next/link";
+import Image from "next/image";
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
+  return (
+    <>
+      <nav className="sticky top-0 bg-white shadow dark:bg-gray-800 z-50">
+        <div className="container px-6 py-3 mx-auto md:flex md:justify-between md:items-center">
+          <div className="flex items-end justify-between">
+            <a href="/"><Image width={100} height={100} src="/img/logo-pmpp.webp" alt="logo" /></a>
+            {/* <a href="/" className="font-bold text-blue-900 lg:pl-2 ">
+              IDADX
+            </a> */}
+
+            {/* <!-- Mobile menu button --> */}
+            <div className="flex lg:hidden">
+              <button
+                onClick={handleClick}
+                type="button"
+                className="text-gray-500 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400 focus:outline-none focus:text-gray-600 dark:focus:text-gray-400"
+                aria-label="toggle menu"
+              >
+                <svg
+                  x-show="!isOpen"
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-6 h-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M4 8h16M4 16h16"
+                  />
+                </svg>
+
+                {/* <svg
+                  x-show="isOpen"
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-6 h-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg> */}
+              </button>
+            </div>
+          </div>
+
+          {/* <!-- Mobile Menu open: "block", Menu closed: "hidden" --> */}
+          <div
+            className={clsx(
+              "absolute inset-x-0 z-20 w-full px-6 py-4 transition-all duration-300 ease-in-out bg-white dark:bg-gray-800 md:mt-0 md:p-0 md:top-0 md:relative md:bg-transparent md:w-auto md:opacity-100 md:translate-x-0 md:flex md:items-center",
+              {
+                "translate-x-0 opacity-100": isOpen,
+                "opacity-0 -translate-x-full": !isOpen,
+              }
+            )}
+          >
+            <div className="flex flex-col md:flex-row md:mx-6">
+              <Link
+                href="/"
+                className="my-2 text-gray-700 transition-colors duration-300 transform dark:text-gray-200 hover:text-teal-500 dark:hover:text-teal-400 md:mx-4 md:my-0"
+              >
+                Home
+              </Link>
+              <Link
+                href="/report"
+                className="my-2 text-gray-700 transition-colors duration-300 transform dark:text-gray-200 hover:text-teal-500 dark:hover:text-teal-400 md:mx-4 md:my-0"
+              >
+                Report Phishing
+              </Link>
+              <Link
+                href="/#faq-section"
+                className="my-2 text-gray-700 transition-colors duration-300 transform dark:text-gray-200 hover:text-teal-500 dark:hover:text-teal-400 md:mx-4 md:my-0"
+              >
+                FAQ
+              </Link>
+
+              <Link
+                href="/register"
+                className="my-2 text-gray-700 transition-colors duration-300 transform dark:text-gray-200 hover:text-teal-500 dark:hover:text-teal-400 md:mx-4 md:my-0"
+              >
+                Join Member
+              </Link>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
