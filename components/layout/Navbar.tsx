7554d80105efebd502202d2b60310d1a62ec93aa
Ahmad Ropai <akhmadrofai@gmail.com>
2023-07-19 19:15:03 +0700

    fix

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -1,149 +1,254 @@
-import { FiInstagram, FiYoutube } from "react-icons/fi";
+import { useState, Fragment } from "react";
+import clsx from "clsx";
+import Link from "next/link";
 import Image from "next/image";
+import { FiInstagram, FiYoutube } from "react-icons/fi";
+import { Menu, Transition } from "@headlessui/react";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
   return (
-    <div className="container mx-auto">
-      <div className="navbar bg-base-100">
-        <div className="navbar-start">
-          <div className="dropdown">
-            <label tabIndex={0} className="btn btn-ghost lg:hidden">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
+    <>
+      <nav className="sticky top-0 bg-white shadow dark:bg-gray-800 z-50">
+        <div className="container px-6 py-3 mx-auto md:flex md:justify-between md:items-center">
+          <div className="flex items-end justify-between">
+            <a href="/">
+              <Image
+                width={30}
+                height={30}
+                src="/img/logo-pmpp.webp"
+                alt="logo"
+              />
+            </a>
+            <a href="/" className="font-bold text-cyan-800 lg:pl-2 ">
+              PMPP
+            </a>
+
+            {/* <!-- Mobile menu button --> */}
+            <div className="flex lg:hidden">
+              <button
+                onClick={handleClick}
+                type="button"
+                className="text-gray-500 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400 focus:outline-none focus:text-gray-600 dark:focus:text-gray-400"
+                aria-label="toggle menu"
               >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
+                <svg
+                  x-show="!isOpen"
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-6 h-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
                   strokeWidth="2"
-                  d="M4 6h16M4 12h8m-8 6h16"
-                />
-              </svg>
-            </label>
-            <ul
-              tabIndex={0}
-              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
-            >
-              <li>
-                <a>Home</a>
-              </li>
-              <li>
-                <a>Info</a>
-                <ul className="p-2">
-                  <li>
-                    <a>Sejarah</a>
-                  </li>
-                  <li>
-                    <a>Tugas Pokok</a>
-                  </li>
-                  <li>
-                    <a>Struktur Organisasi</a>
-                  </li>
-                  <li>
-                    <a>Komandan dari masa ke masa</a>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <a>Aktifitas</a>
-                <ul className="p-2">
-                  <li>
-                    <a>Kegiatan PMPP TNI</a>
-                  </li>
-                  <li>
-                    <a>Kegiatan Kontingen Garuda</a>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <a>Tentang Kami</a>
-              </li>
-              <li>
-                <a>Hubungi Kami</a>
-              </li>
-            </ul>
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M4 8h16M4 16h16"
+                  />
+                </svg>
+
+                {/* <svg
+                  x-show="isOpen"
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-6 h-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg> */}
+              </button>
+            </div>
           </div>
-          <Image
-            width={100}
-            height={100}
-            src="/img/logo-pmpp.webp"
-            alt="logo"
-            className="lg:w-10 w-10"
-          />
-          <a className="btn btn-ghost normal-case text-xl text-cyan-700">
-            PMPP
-          </a>
-        </div>
-        <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">
-            <li>
-              <a>Home</a>
-            </li>
-            <li tabIndex={0}>
-              <details>
-                <summary>Info</summary>
-                <ul className="p-2">
-                  <li>
-                    <a>Sejarah</a>
-                  </li>
-                  <li>
-                    <a>Tugas Pokok</a>
-                  </li>
-                  <li>
-                    <a>Struktur Organisasi</a>
-                  </li>
-                  <li>
-                    <a>Komandan dari masa ke masa</a>
-                  </li>
-                </ul>
-              </details>
-            </li>
-            <li tabIndex={0}>
-              <details>
-                <summary>Aktifitas</summary>
-                <ul className="p-2">
-                  <li>
-                    <a>Kegiatan PMPP TNI</a>
-                  </li>
-                  <li>
-                    <a>Kegiatan Kontingen Garuda</a>
-                  </li>
-                </ul>
-              </details>
-            </li>
-            <li>
-              <a>Tentang Kami</a>
-            </li>
-            <li>
-              <a>Hubungi Kami</a>
-            </li>
-          </ul>
-        </div>
-        <div className="navbar-end gap-2">
-          <a
-            href="/"
-            className="text-gray-500 transition-colors duration-300 hover:text-teal-400"
+
+          {/* <!-- Mobile Menu open: "block", Menu closed: "hidden" --> */}
+          <div
+            className={clsx(
+              "absolute inset-x-0 z-20 w-full px-6 py-4 transition-all duration-300 ease-in-out bg-white dark:bg-gray-800 md:mt-0 md:p-0 md:top-0 md:relative md:bg-transparent md:w-auto md:opacity-100 md:translate-x-0 md:flex md:items-center",
+              {
+                "translate-x-0 opacity-100": isOpen,
+                "opacity-0 -translate-x-full": !isOpen,
+              }
+            )}
           >
-            <FiYoutube
-              size={24}
-              className="stroke-current hover:text-teal-400"
-            />
-          </a>
-          <a
-            href="/"
-            className="text-gray-500 transition-colors duration-300 hover:text-teal-400"
-          >
-            <FiInstagram
-              size={22}
-              className="stroke-current hover:text-teal-400"
-            />
-          </a>
+            <div className="flex flex-col md:flex-row md:mx-6 gap-4">
+              <Link
+                href="/"
+                className="my-2 text-gray-700 transition-colors duration-300 transform dark:text-gray-200 hover:text-teal-500 dark:hover:text-teal-400 md:my-0"
+              >
+                Home
+              </Link>
+              <Menu as="div" className="relative inline-block text-left">
+                <div>
+                  <Menu.Button className="inline-flex w-full justify-center text-gray-700">
+                    Info
+                  </Menu.Button>
+                </div>
+                <Transition
+                  as={Fragment}
+                  enter="transition ease-out duration-100"
+                  enterFrom="transform opacity-0 scale-95"
+                  enterTo="transform opacity-100 scale-100"
+                  leave="transition ease-in duration-75"
+                  leaveFrom="transform opacity-100 scale-100"
+                  leaveTo="transform opacity-0 scale-95"
+                >
+                  <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                    <div className="px-1 py-1 ">
+                      <Menu.Item>
+                        {({ active }) => (
+                          <Link
+                            href="#"
+                            className={`${
+                              active
+                                ? "bg-violet-500 text-white"
+                                : "text-gray-900"
+                            } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                          >
+                            Sejarah
+                          </Link>
+                        )}
+                      </Menu.Item>
+                      <Menu.Item>
+                        {({ active }) => (
+                          <Link
+                            href="#"
+                            className={`${
+                              active
+                                ? "bg-violet-500 text-white"
+                                : "text-gray-900"
+                            } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                          >
+                            Tugas Pokok
+                          </Link>
+                        )}
+                      </Menu.Item>
+                      <Menu.Item>
+                        {({ active }) => (
+                          <Link
+                            href="#"
+                            className={`${
+                              active
+                                ? "bg-violet-500 text-white"
+                                : "text-gray-900"
+                            } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                          >
+                            Struktur Organisasi
+                          </Link>
+                        )}
+                      </Menu.Item>
+                      <Menu.Item>
+                        {({ active }) => (
+                          <Link
+                            href="#"
+                            className={`${
+                              active
+                                ? "bg-violet-500 text-white"
+                                : "text-gray-900"
+                            } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                          >
+                            Komandan dari masa ke masa
+                          </Link>
+                        )}
+                      </Menu.Item>
+                    </div>
+                  </Menu.Items>
+                </Transition>
+              </Menu>
+              <Menu as="div" className="relative inline-block text-left">
+                <div>
+                  <Menu.Button className="inline-flex w-full justify-center text-gray-700">
+                    Aktifitas
+                  </Menu.Button>
+                </div>
+                <Transition
+                  as={Fragment}
+                  enter="transition ease-out duration-100"
+                  enterFrom="transform opacity-0 scale-95"
+                  enterTo="transform opacity-100 scale-100"
+                  leave="transition ease-in duration-75"
+                  leaveFrom="transform opacity-100 scale-100"
+                  leaveTo="transform opacity-0 scale-95"
+                >
+                  <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                    <div className="px-1 py-1 ">
+                      <Menu.Item>
+                        {({ active }) => (
+                          <Link
+                            href="#"
+                            className={`${
+                              active
+                                ? "bg-violet-500 text-white"
+                                : "text-gray-900"
+                            } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                          >
+                            Kegiatan PMPP TNI
+                          </Link>
+                        )}
+                      </Menu.Item>
+                      <Menu.Item>
+                        {({ active }) => (
+                          <Link
+                            href="#"
+                            className={`${
+                              active
+                                ? "bg-violet-500 text-white"
+                                : "text-gray-900"
+                            } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                          >
+                            Kegiatan Kontingen Garuda
+                          </Link>
+                        )}
+                      </Menu.Item>
+                    </div>
+                  </Menu.Items>
+                </Transition>
+              </Menu>
+
+              <Link
+                href="/register"
+                className="my-2 text-gray-700 transition-colors duration-300 transform dark:text-gray-200 hover:text-teal-500 dark:hover:text-teal-400 md:mx-4 md:my-0"
+              >
+                About Us
+              </Link>
+              <Link
+                href="/register"
+                className="my-2 text-gray-700 transition-colors duration-300 transform dark:text-gray-200 hover:text-teal-500 dark:hover:text-teal-400 md:mx-4 md:my-0"
+              >
+                Hubungi Kami
+              </Link>
+              <a
+                href="/"
+                className="text-gray-500 transition-colors duration-300 hover:text-teal-400"
+              >
+                <FiYoutube
+                  size={24}
+                  className="stroke-current hover:text-teal-400 mr-2"
+                />
+              </a>
+              <a
+                href="/"
+                className="text-gray-500 transition-colors duration-300 hover:text-teal-400"
+              >
+                <FiInstagram
+                  size={22}
+                  className="stroke-current hover:text-teal-400 mr-2"
+                />
+              </a>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      </nav>
+    </>
   );
 };
 
